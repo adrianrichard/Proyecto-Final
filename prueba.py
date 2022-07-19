cf563e315abe9fc08e0897e5a3fd98cd09ab9eb0
unknown <rodrigoadrianrichard@gmail.com>
2022-07-18 21:52:18 -0300

    prueba de subida 2

--- prueba.py
+++ prueba.py
@@ -1,1 +1,10 @@
-print("Python")
+import Tkinter
+
+def funcion():
+    otra_ventana = Tkinter.Toplevel(root)
+    root.iconify()
+
+root = Tkinter.Tk()
+boton = Tkinter.Button(root, text="Abrir otra ventana", command=funcion)
+boton.pack()
+root.mainloop()
