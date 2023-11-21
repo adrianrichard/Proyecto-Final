ff8058f87d66c13f3c2a75d36a1d97811ed5ae25
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2023-11-21 14:51:01 -0300

    CRUD
    armando el CRUD

---
+++ crud.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+def main():
+    root=tk.Tk()
+    root.title('CRUD paciente')
+    root.resizable(0,0)
+    frame=tk.Frame(root)
+    frame.pack()
+    frame.config(width=480, height=320, bg='green')
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
