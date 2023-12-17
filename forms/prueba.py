0ea5912249d4733d638d7a075405c747171891e7
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2023-12-17 20:41:46 -0300

    arreglar Entry

--- forms/prueba.py
+++ forms/prueba.py
@@ -3,11 +3,18 @@ from tkinter import *
 from tkinter.font import BOLD
 from tkinter import  messagebox, Button, Label, Frame
 import sqlite3
+nombre_paciente = StringVar()
+apellido_paciente = StringVar()
+dni_paciente =  StringVar()
+domicilio_paciente =  StringVar()
+telefono_paciente =  StringVar()
+email_paciente =  StringVar()
+obrasocial_paciente =  StringVar()
+nrosocio_paciente =  StringVar()
+class Paciente():
 
-class Paciente():    
+    def __init__(self):
 
-    def __init__(self):        
-        
         self.frame_paciente= tk.Tk()
         self.frame_paciente.title('DentalMatic')
         self.frame_paciente.geometry('1000x500')
@@ -18,21 +25,14 @@ class Paciente():
         self.color = True
         self.frame_top = Frame(self.frame_paciente, bg= '#1F704B', height= 50)
 
-        self.frame_top.grid(column= 1, row= 0, sticky= 'nsew')        
+        self.frame_top.grid(column= 1, row= 0, sticky= 'nsew')
         self.frame_principal = Frame(self.frame_paciente)
-        self.frame_principal.grid(column= 1, row= 1, sticky= 'nsew')		
+        self.frame_principal.grid(column= 1, row= 1, sticky= 'nsew')
 
         self.titulo = Label(self.frame_top, text= 'Paciente', bg= '#1F704B', fg= 'white', font= ('Comic Sans MS', 15, 'bold')).grid(column= 1, row=0, pady= 20, padx= 10)
         Button(self.frame_top, text= 'Cerrar',  font= ('Comic Sans MS', 15, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 2, row=0, pady= 20, padx= 500)
-        nombre_paciente = StringVar()
-        apellido_paciente = StringVar()
-        dni_paciente =  StringVar()
-        domicilio_paciente =  StringVar()
-        telefono_paciente =  StringVar()
-        email_paciente =  StringVar()
-        obrasocial_paciente =  StringVar()
-        nrosocio_paciente =  StringVar()
-    
+
+
         conexionBBDD()
 
         #Entradas Y ETIQUETAS DATOS DEL PACIENTE
@@ -44,7 +44,7 @@ class Paciente():
         Entry(self.frame_principal, textvariable=email_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=6, pady=5, padx=10)
         Entry(self.frame_principal, textvariable=obrasocial_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=7, pady=5, padx=10)
         Entry(self.frame_principal, textvariable=nrosocio_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=8, pady=5, padx=10)
-        
+
         Label(self.frame_principal, text= 'Nombre/s',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=1, pady=5, padx=2)
         Label(self.frame_principal, text= 'Apellido/s',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=2, pady=5, padx=2)
         Label(self.frame_principal, text= 'D.N.I.',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=3, pady=5, padx=2)
@@ -53,13 +53,13 @@ class Paciente():
         Label(self.frame_principal, text= 'Email', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=6, pady=5, padx=2)
         Label(self.frame_principal, text= 'Obra Social', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=7, pady=5, padx=2)
         Label(self.frame_principal, text= 'Nro de socio', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=8, pady=5, padx=2)
-        Button(self.frame_principal, text= 'Cerrar',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 3, row=1, pady= 5, padx= 200)
+        Button(self.frame_principal, text= 'Crear',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= crear).grid(column= 3, row=1, pady= 5, padx= 200)
         Button(self.frame_principal, text= 'Cancelar',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 3, row=2, pady= 5, padx= 200)
-        Button(self.frame_principal, text= 'Limpiar datos',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 3, row=2, pady= 5, padx= 200)
+        Button(self.frame_principal, text= 'Limpiar datos',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 3, row=3, pady= 5, padx= 200)
 
         self.frame_paciente.mainloop()
 def conexionBBDD():
-	miConexion=sqlite3.connect("./bd/DBpaciente.sqlite3")
+	miConexion=sqlite3.connect("../bd/DBpaciente.sqlite3")
 	miCursor=miConexion.cursor()
 
 	try:
@@ -72,26 +72,30 @@ def conexionBBDD():
 			DOMICILIO VARCHAR(50) NOT NULL,
             TELEFONO INT NOT NULL,
 			EMAIL VARCHAR(50) NOT NULL,
-            OBRASOCIAL VARCHAR(50) NOT NULL, 
+            OBRASOCIAL VARCHAR(50) NOT NULL,
             NROSOCIO INT NOT NULL)
 			''')
+
 		messagebox.showinfo("CONEXION","Base de Datos Creada exitosamente")
+
 	except:
 		messagebox.showinfo("CONEXION", "Conexión exitosa con la base de datos")
 
+
 def crear():
-	miConexion=sqlite3.connect("base")
+	miConexion=sqlite3.connect("../bd/DBpaciente.sqlite3")
 	miCursor=miConexion.cursor()
- 
-	try:
-		datos=nombre_paciente.get(),apellido_paciente.get(),dni_paciente.get(), domicilio_paciente.get(),telefono_paciente.get(),email_paciente.get(),obrasocial_paciente.get(),nrosocio_paciente.get()
-		miCursor.execute("INSERT INTO Paciente VALUES(NULL,?,?,?,?,?,?,?,?)", (datos))
-		miConexion.commit()
-	except:
-		messagebox.showwarning("ADVERTENCIA","Ocurrió un error al crear el registro, verifique conexión con BBDD")
-def only_numbers(char):
-    return char.isdigit()    
-		
+
+	#try:
+	datos=nombre_paciente.get(),apellido_paciente.get(),dni_paciente.get(), domicilio_paciente.get(),telefono_paciente.get(),email_paciente.get(),obrasocial_paciente.get(),nrosocio_paciente.get()
+	miCursor.execute("INSERT INTO Paciente VALUES(NULL,?,?,?,?,?,?,?,?)", (datos))
+	miConexion.commit()
+    #except:
+    #    print('nada')
+
+#def only_numbers(char):
+    #return char.isdigit()
+
 if __name__ == "__main__":
     Paciente()
 
