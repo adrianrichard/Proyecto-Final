05dc64db91f30774b49ac834b2f52b7601ff7983
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2024-01-16 10:37:18 -0300

    Agregar paciente
    Guardar el paciente. Cartel de salir sin guardar

--- CRUD/crud3.py
+++ CRUD/crud3.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 from tkinter.font import BOLD
 from tkinter import  messagebox, Button, Label, Frame
 import sqlite3
@@ -30,14 +29,18 @@ class Paciente():
         self.miConexion=sqlite3.connect("./bd/DBpaciente.sqlite3")
         self.miCursor=self.miConexion.cursor()
         datos=self.nombre_paciente.get(), self.apellido_paciente.get(), self.dni_paciente.get(), self.domicilio_paciente.get(),self.telefono_paciente.get(),self.email_paciente.get(),self.obrasocial_paciente.get(),self.nrosocio_paciente.get()
-        print(datos)
+        #print(datos)
         try:
             self.miCursor.execute("INSERT INTO Paciente VALUES(NULL,?,?,?,?,?,?,?,?)", (datos))
             self.miConexion.commit()
             messagebox.showinfo("GUARDAR","Paciente guardado exitosamente")
         except:
             messagebox.showinfo("GUARDAR", "No se ha podido guardar el paciente")
-        
+    
+    def Salir(self): 
+        answer = messagebox.askokcancel(title='Salir', message='¿Desea salir sin guardar?', icon='warning')
+        if answer:
+            self.frame_paciente.destroy()      
 
 
     def __init__(self, *args, **kwargs):
@@ -66,10 +69,10 @@ class Paciente():
         self.nrosocio_paciente =  StringVar()
         self.conexionBBDD()
         self.crear_w()
-    
+
     def crear_w(self):
         self.titulo = Label(self.frame_top, text= 'Paciente', bg= '#1F704B', fg= 'white', font= ('Comic Sans MS', 15, 'bold')).grid(column= 1, row=0, pady= 20, padx= 10)
-        Button(self.frame_top, text= 'Cerrar',  font= ('Comic Sans MS', 15, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 2, row=0, pady= 20, padx= 500)
+        Button(self.frame_top, text= 'Cerrar',  font= ('Comic Sans MS', 15, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.Salir).grid(column= 2, row=0, pady= 20, padx= 500)
 
         #Entradas Y ETIQUETAS DATOS DEL PACIENTE
         Entry(self.frame_principal, textvariable=self.nombre_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=1, pady=5, padx=10)
@@ -90,7 +93,7 @@ class Paciente():
         Label(self.frame_principal, text= 'Obra Social', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=7, pady=5, padx=2)
         Label(self.frame_principal, text= 'Nro de socio', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=8, pady=5, padx=2)
         Button(self.frame_principal, text= 'Crear',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.crear).grid(column= 3, row=1, pady= 5, padx= 200)
-        Button(self.frame_principal, text= 'cargar',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.conexionBBDD).grid(column= 3, row=2, pady= 5, padx= 200)
+        Button(self.frame_principal, text= 'Salir',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.Salir).grid(column= 3, row=2, pady= 5, padx= 200)
         Button(self.frame_principal, text= 'Crear',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.crear).grid(column= 3, row=3, pady= 5, padx= 200)
 
         self.frame_paciente.mainloop()
--- forms/form_paciente.py
+++ forms/form_paciente.py
@@ -1,100 +1,103 @@
 import tkinter as tk
 from tkinter.font import BOLD
-import util.generic as utl
-from tkinter import  Tk, Button, Entry, Label, ttk, PhotoImage
-from tkinter import  StringVar,Scrollbar,Frame
-from bd.conexion import Conexion
+#import util.generic as utl
+from tkinter import messagebox, Button, Entry, Label #, PhotoImage
+from tkinter import  StringVar, Frame
+#from bd.conexion import Conexion
+import sqlite3
+
 
 class Paciente:    
-                                      
-    def __init__(self):        
+    
+    def conexionBBDD(self):
+        
+        try:
+            self.miConexion=sqlite3.connect("./bd/DBpaciente.sqlite3")
+            self.miCursor=self.miConexion.cursor()
+            
+        except:
+            self.miCursor.execute('''
+                CREATE TABLE Paciente (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NOMBRE VARCHAR(50) NOT NULL,
+                APELLIDO VARCHAR(50) NOT NULL)
+                ''')
+            #self.miConexion.commit()
+            #self.miConexion.close()
+
+            messagebox.showinfo("CONEXION","Base de Datos Creada exitosamente")
+    
+    def guardar(self):
+        self.miConexion=sqlite3.connect("./bd/DBpaciente.sqlite3")
+        self.miCursor=self.miConexion.cursor()
+        datos=self.nombre_paciente.get(), self.apellido_paciente.get(), self.dni_paciente.get(), self.domicilio_paciente.get(),self.telefono_paciente.get(),self.email_paciente.get(),self.obrasocial_paciente.get(),self.nrosocio_paciente.get()
+        #print(datos)
+        try:
+            self.miCursor.execute("INSERT INTO Paciente VALUES(NULL,?,?,?,?,?,?,?,?)", (datos))
+            self.miConexion.commit()
+            messagebox.showinfo("GUARDAR","Paciente guardado exitosamente")
+        except:
+            messagebox.showinfo("GUARDAR", "No se ha podido guardar el paciente")
+    
+    def Salir(self): 
+        answer = messagebox.askokcancel(title='Salir', message='¿Desea salir sin guardar?', icon='warning')
+        if answer:
+            self.frame_paciente.destroy()
+                                                 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
         self.frame_paciente= tk.Toplevel()
         self.frame_paciente.grab_set_global() # Obliga a las ventanas estar deshabilitadas y deshabilitar todos los eventos e interacciones con la ventana
-        self.frame_paciente.focus_set() # Mantiene el foco cuando se abre la ventana.
+        self.frame_paciente.focus_set() # Mantiene el foco cuando se abre la ventana.        
+
         self.frame_paciente.title('DentalMatic')
-        self.frame_paciente.geometry('1000x500')
+        self.frame_paciente.geometry('800x500')
         self.frame_paciente.config(bg='#fcfcfc')
         self.frame_paciente.resizable(width= 0, height= 0)
-        utl.centrar_ventana(self.frame_paciente, 900, 600)
+        #utl.centrar_ventana(self.frame_paciente, 900, 600)
         self.menu = True
         self.color = True
         self.frame_top = Frame(self.frame_paciente, bg= '#1F704B', height= 50)
 
-        self.frame_top.grid(column= 1, row= 0, sticky= 'nsew')        
-        self.frame_principal = Frame(self.frame_paciente, bg= 'white')
-        self.frame_principal.grid(column= 1, row= 1, sticky= 'nsew')		
-        self.frame_paciente.columnconfigure(1, weight= 1)
-        self.frame_paciente.rowconfigure(1, weight= 1)
-        self.frame_principal.columnconfigure(1, weight= 1)
-        self.frame_principal.rowconfigure(1, weight= 1)
-        self.titulo = Label(self.frame_top, text= 'Consultorio Odóntologico MyM', bg= '#1F704B', fg= 'white', font= ('Comic Sans MS', 15, 'bold')).grid(column= 1, row=0, pady= 20, padx= 10)
-        Button(self.frame_top, text= 'Cerrar',  font= ('Comic Sans MS', 15, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.frame_paciente.destroy).grid(column= 2, row=0, pady= 20, padx= 500)
-        #BOTONES Y ETIQUETAS DATOS DEL PACIENTE
-        Entry(self.frame_principal, font= ('Comic Sans MS', 14)).grid(column=0, row=1, pady=20, padx=100)
-        #Entry(self.frame_principal, text= 'Pacientes', fg= 'white', bg='black',activebackground='black', bd=0 ).grid(column=0, row=2, pady=20,padx=10)
-        #Entry(self.frame_principal, text= 'Pacientes', fg= 'white', bg= 'black',activebackground='black', bd=0 ).grid(column=0, row=3, pady=20,padx=10)
-        Button(self.frame_principal, text= 'Pacientes', fg= 'white', bg= 'black',activebackground='black', bd=0).grid(column=0, row=4, pady=20, padx=10)
-        Button(self.frame_principal, text= 'Pacientes', fg= 'white', bg= 'black',activebackground='black', bd=0).grid(column=0, row=5, pady=20, padx=10)
-
-        Label(self.frame_principal, text= 'Pacientes', bg= 'black', fg= 'white', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=1, pady=20, padx=2)
-        Label(self.frame_principal, text= 'Calendario', bg= 'black', fg= 'white', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=2, pady=20, padx=2)
-        Label(self.frame_principal, text= 'Historia \nClinica', bg= 'black', fg= 'white', font= ('Comic Sans MS', 12, 'bold')).grid(column=1, row=3, pady=20, padx=2)
-        Label(self.frame_principal, text= 'Eliminar', bg= 'black', fg= 'white', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=4, pady=20, padx=2)
-        Label(self.frame_principal, text= 'Versión', bg= 'black', fg= 'white', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=5, pady=20, padx=2)
+        self.frame_top.grid(column= 1, row= 0, sticky= 'nsew')
+        self.frame_principal = Frame(self.frame_paciente)
+        self.frame_principal.grid(column= 1, row= 1, sticky= 'nsew')
+        self.nombre_paciente = StringVar()
+        self.apellido_paciente = StringVar()
+        self.dni_paciente =  StringVar()
+        self.domicilio_paciente =  StringVar()
+        self.telefono_paciente =  StringVar()
+        self.email_paciente =  StringVar()
+        self.obrasocial_paciente =  StringVar()
+        self.nrosocio_paciente =  StringVar()
+        self.conexionBBDD()
         
+        self.titulo = Label(self.frame_top, text= 'Paciente', bg= '#1F704B', fg= 'white', font= ('Comic Sans MS', 15, 'bold')).grid(column= 1, row=0, pady= 20, padx= 10)
+        Button(self.frame_top, text= 'Cerrar',  font= ('Comic Sans MS', 15, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.Salir).grid(column= 2, row=0, pady= 20, padx= 500)
+
+        #Entradas Y ETIQUETAS DATOS DEL PACIENTE
+        Entry(self.frame_principal, textvariable=self.nombre_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=1, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.apellido_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=2, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.dni_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=3, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.domicilio_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=4, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.telefono_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=5, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.email_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=6, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.obrasocial_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=7, pady=5, padx=10)
+        Entry(self.frame_principal, textvariable=self.nrosocio_paciente, font= ('Comic Sans MS', 14)).grid(column=1, row=8, pady=5, padx=10)
+
+        Label(self.frame_principal, text= 'Nombre/s',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=1, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Apellido/s',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=2, pady=5, padx=2)
+        Label(self.frame_principal, text= 'D.N.I.',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=3, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Domicilio',  fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=4, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Telefono', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=5, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Email', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=6, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Obra Social', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=7, pady=5, padx=2)
+        Label(self.frame_principal, text= 'Nro de socio', fg= 'black', font= ('Comic Sans MS', 12, 'bold')).grid(column=0, row=8, pady=5, padx=2)
+        Button(self.frame_principal, text= 'Crear',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.guardar).grid(column= 3, row=1, pady= 5, padx= 200)
+        Button(self.frame_principal, text= 'cargar',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0 ).grid(column= 3, row=2, pady= 5, padx= 200)
+        #Button(self.frame_principal, text= 'Crear',  font= ('Comic Sans MS', 12, BOLD), fg= 'white', bg= '#1F704B', activebackground= 'gray', bd= 0, command= self.crear).grid(column= 3, row=3, pady= 5, padx= 200)
+
         self.frame_paciente.mainloop()
-
-    #def pantalla_inicial(self):
-    
-
-    #def pantalla_datos(self):
-             
-    #def pantalla_escribir(self):
-     
-    #def pantalla_actualizar(self):
         
-    
-    #def pantalla_buscar(self):
-
-   # def pantalla_ajustes(self):
-    
-    #def agregar_paciente():
-        
-		
-
 if __name__ == "__main__":
     Paciente()
-    #def agregar_paciente():
-        
-
-    '''    estilo_paginas = ttk.Style()
-        estilo_paginas.configure("TNotebook", background='black', foreground='gray', padding=0, borderwidth=0)
-        estilo_paginas.theme_use('default')
-        estilo_paginas.configure("TNotebook", background='black', borderwidth=0)
-        estilo_paginas.configure("TNotebook.Tab", background="black", borderwidth=0)
-        estilo_paginas.map("TNotebook", background=[("selected", 'black')])
-        estilo_paginas.map("TNotebook.Tab", background=[("selected", 'black')], foreground=[("selected", 'gray')]);
-
-		#CREACCION DE LAS PAGINAS
-        self.paginas = ttk.Notebook(self.frame_principal , style= 'TNotebook') #, style = 'TNotebook'
-        self.paginas.grid(column=0,row=0, sticky='nsew')
-        self.frame_uno = Frame(self.paginas, bg='white') #color de fondo
-        self.frame_dos = Frame(self.paginas, bg='white') #color de fondo
-        self.frame_tres = Frame(self.paginas, bg='white')
-        self.frame_cuatro = Frame(self.paginas, bg='white')
-        self.frame_cinco = Frame(self.paginas, bg='white')
-        self.frame_seis = Frame(self.paginas, bg='white')
-        self.paginas.add(self.frame_uno)
-        self.paginas.add(self.frame_dos)
-        self.paginas.add(self.frame_tres)
-        self.paginas.add(self.frame_cuatro)
-        self.paginas.add(self.frame_cinco)
-        self.paginas.add(self.frame_seis)
-	'''	
-  ##############################         PAGINAS       #############################################
-		######################## FRAME TITULO #################
-        
-
-		######################## VENTANA PRINCIPAL #################
-        #Label(self.frame_principal, image= self.logo, bg='white').pack(expand= 1)
-
