c5bf260c763cab5791e4a08b3e49ebb958c87582
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2024-05-31 13:05:46 -0300

    TURNOS - Carga de tabla
    La carga funciona, se accede a la BD

--- Extras/tablaturnos2.py
+++ Extras/tablaturnos2.py
@@ -4,7 +4,7 @@ from tkinter import *
 from datetime import datetime, timedelta
 import sqlite3
 from tkinter import  messagebox, Frame
-from editarturno import Editar
+#from editarturno import Editar
 
 class Turno:
     
@@ -16,17 +16,79 @@ class Turno:
         
         self.widgets()
     
+    def cargar_turnos(self):
+        self.conn= sqlite3.connect('turnos.db')
+        self.cur= self.conn.cursor()
+        print('probando')
+        # Leer los datos de la tabla
+        self.cur.execute('SELECT * FROM turnos ORDER BY hora')
+        turnos = self.cur.fetchall()
+        # Mostrar los datos
+        # for turno in turnos:
+        #     print(turno)
+        # Cerrar la conexión
+        self.conn.close()
+        return turnos
+
+    def agregar_turno(self):
+        print("guardado")
+    
+    def cancelar_turno(self):
+        self.ventana_secundaria.destroy()
+        
     def abrir_ventana_secundaria(self, event):
-        #print('hola')
+        self.ventana_secundaria = tk.Toplevel(self.ventana_turno, background='gray')
+        self.ventana_secundaria.title("Ventana Secundaria")
+        self.ventana_secundaria.geometry('400x300')
+        
+        # #print('hola')
         item = self.tabla_turnos.focus()
         self.data = self.tabla_turnos.item(item)
         #print(self.data)
-        valores = self.data['values'] #VALORES DE LA TABLA
-        selected_item = self.tabla_turnos.selection()[0]
-        self.tabla_turnos.item(selected_item, text="blub", values=(valores[0], "Pedro", "Pedro", "Pedro"))
+        Label(self.ventana_secundaria, text="EDITAR TURNO", font=("Arial", 15, 'bold'), bg="gray90", width=60).pack(pady=10)
+        self.nombre_entry = Entry(self.ventana_secundaria, justify=CENTER)
+        self.nombre_entry.pack(pady=8)
+        self.nombre_entry.insert(0, "Paciente")
+        self.nombre_entry.focus_set()
+        prestaciones = ["Consulta", "Extracción", "Tratamiento de conducto", "Reparación"]
+        self.selector_prestacion = ttk.Combobox(self.ventana_secundaria, state="readonly", values=prestaciones, width=25, justify=CENTER, background="white")
+        self.selector_prestacion.pack(pady=8)
+        self.selector_prestacion.set("Prestación")
+        self.selector_prestacion.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        odontologos = ["Militello", "Macua", "Ramirez"]
+        self.selector_odontologo= ttk.Combobox(self.ventana_secundaria, state="readonly", values=odontologos, width=25, justify=CENTER, background="white")
+        self.selector_odontologo.pack(pady=8)
+        self.selector_odontologo.set("Odontólogo")
+        self.selector_odontologo.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        
+        tiempo_frame = Frame(self.ventana_secundaria)
+        tiempo_frame.pack(pady=8)
+
+        horas = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        self.selector_hora = ttk.Combobox(tiempo_frame, values=horas, state="readonly", justify=CENTER, background="white", width=10)
+        self.selector_hora.set("Hora")
+        self.selector_hora.grid(row=0, column=0)
+
+        minutos = ["00","30"]
+        #minutos.extend([str(num * 10) for num in range(1, 6)])
+        self.selector_minuto = ttk.Combobox(tiempo_frame, state="readonly", values=minutos, justify=CENTER, background="white", width=10)
+        self.selector_minuto.set("00")
+        self.selector_minuto.grid(row=0, column=1, sticky=E)
+        self.selector_hora.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        self.selector_minuto.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        
+        button_frame = Frame(self.ventana_secundaria, bg="gray")
+        button_frame.pack(pady=10)
+        
+        Button(button_frame, text='confirm', command=self.agregar_turno, bg="#BDC1BE").grid(row=0, column=0, padx=10)
+        Button(button_frame, text='cancelar',  command=self.cancelar_turno, bg="#BDC1BE").grid(row=0, column=1, padx = 10)
+        
+        #valores = self.data['values'] #VALORES DE LA TABLA
+        #selected_item = self.tabla_turnos.selection()[0]
+        #self.tabla_turnos.item(selected_item, text="blub", values=(valores[0], "Pedro", "Pedro", "Pedro"))
         #print(valores)
-        editar=Editar()
-        editar.prueba(valores)
+        #editar=Editar()
+        #editar.prueba(valores)
         
     def widgets(self):
         self.frame_tabla = ttk.Frame(self.ventana_turno)
@@ -46,15 +108,21 @@ class Turno:
         self.tabla_turnos.column("Paciente", width=100)
         self.tabla_turnos.column("Prestacion", width=100)
         self.tabla_turnos.column("Odontologo", width=100)
+        turnos_dados=self.cargar_turnos()        
         # cargar filas al Treeview
         start_time = datetime.strptime("08:00", "%H:%M")
         # Intervalo de 30 minutos
         time_interval = timedelta(minutes=30)
         for i in range(0, 25):
             current_time = start_time + i * time_interval
-            self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), '', '', ''))
+            for turno in turnos_dados:
+                if(current_time.strftime("%H:%M") == turno[3]):
+                    self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), turno[4], turno[5], turno[6]))
+            else:
+                self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), '', '', ''))
         self.tabla_turnos.bind("<Double-1>", self.abrir_ventana_secundaria)
        #self.frame_tabla.grid_columnconfigure(0, weight=1)
+        
         self.ventana_turno.mainloop()
         
     # def crear_BD(self):
