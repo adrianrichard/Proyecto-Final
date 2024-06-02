4183aadd49c7c66912968561f3cd1756c853026e
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2024-06-02 13:47:12 -0300

    Turnos
    Carga andando
    Se obtienen los valores de los combobox
    La hora ya está predefinida
    Agregar el guardado en la base de datos

--- Extras/tablaturnos2.py
+++ Extras/tablaturnos2.py
@@ -20,18 +20,17 @@ class Turno:
         try:
             self.conn= sqlite3.connect('turnos.db')
             self.cur= self.conn.cursor()
-            print('probando')
+            # Leer los datos de la tabla
+            self.cur.execute('SELECT * FROM turnos ORDER BY hora')
+            turnos_dados = self.cur.fetchall()
+            self.conn.close()
         except:
             print('no abre BD')
-        # Leer los datos de la tabla
-        self.cur.execute('SELECT * FROM turnos ORDER BY hora')
-        turnos_dados = self.cur.fetchall()
-        # Mostrar los datos
-        # for turno in turnos:
-        #     print(turno)
+                
         # Cerrar la conexión
-        self.conn.close()
+        
         self.tabla_turnos.delete(*self.tabla_turnos.get_children())
+                
         start_time = datetime.strptime("08:00", "%H:%M")
         # Intervalo de 30 minutos
         time_interval = timedelta(minutes=30)
@@ -39,14 +38,14 @@ class Turno:
             current_time = start_time + i * time_interval
             for turno in turnos_dados:
                 if(current_time.strftime("%H:%M") == turno[3]):
-                    self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), turno[4], turno[5], turno[6]), tags='anotado')
-                    self.style.configure('Item', foreground = 'red', focuscolor ='green')
+                    self.tabla_turnos.tag_configure('anotado', font=("Arial", 10, 'bold'), background="sky blue")
+                    self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), turno[4], turno[5], turno[6]), tags=('anotado',))                    
             else:
-                self.tabla_turnos.insert("", "end", values=(current_time.strftime("%H:%M"), '', '', ''))
+                self.tabla_turnos.insert(parent='', index='end', values=(current_time.strftime("%H:%M"), '', '', ''))
         #return turnos
 
-    def agregar_turno(self):
-        print("guardado")
+    # def agregar_turno(self):
+    #     print("guardado")
     
     def cancelar_turno(self):
         self.ventana_secundaria.destroy()
@@ -55,50 +54,88 @@ class Turno:
         self.ventana_secundaria = tk.Toplevel(self.ventana_turno, background='gray')
         self.ventana_secundaria.title("Ventana Secundaria")
         self.ventana_secundaria.geometry('400x300')
-        
+        self.ventana_secundaria.grab_set_global() # Obliga a las ventanas estar deshabilitadas y deshabilitar todos los eventos e interacciones con la ventana
+        self.ventana_secundaria.focus_set() # Mantiene el foco cuando se abre la ventana.
         # #print('hola')
-        item = self.tabla_turnos.focus()
-        #self.data = self.tabla_turnos.item(item)
+        #item = self.tabla_turnos.focus()
+        
         self.turno_seleccionado = self.tabla_turnos.selection()[0]
         #print(self.turno_seleccionado)
-        #print(self.data)
+        self.data = self.tabla_turnos.item(self.turno_seleccionado)
+        self.horario = self.data['values'][0]
+        self.paciente = self.data['values'][1]
+        self.prestacion = self.data['values'][2]
+        self.odontologo = self.data['values'][3]
         Label(self.ventana_secundaria, text="EDITAR TURNO", font=("Arial", 15, 'bold'), bg="gray90", width=60).pack(pady=10)
+        Label(self.ventana_secundaria, text="FECHA: DD/MM/AAAA   -"+"   HORARIO: "+self.horario, font=("Arial", 10, 'bold'), bg="gray90", width=60).pack()
+
         self.nombre_entry = Entry(self.ventana_secundaria, justify=CENTER)
-        self.nombre_entry.pack(pady=8)
+        self.nombre_entry.pack(pady=(10,10))        
         self.nombre_entry.insert(0, "Paciente")
+        if (self.paciente != ''):
+            self.nombre_entry.delete(0, END)
+            self.nombre_entry.insert(0, self.paciente)
         self.nombre_entry.focus_set()
-        prestaciones = ["Consulta", "Extracción", "Tratamiento de conducto", "Reparación"]
+        prestaciones = ["Consulta", "Extracción", "Tratamiento de conducto", "Reparación", "Limpieza"]
         self.selector_prestacion = ttk.Combobox(self.ventana_secundaria, state="readonly", values=prestaciones, width=25, justify=CENTER, background="white")
         self.selector_prestacion.pack(pady=8)
         self.selector_prestacion.set("Prestación")
+        if (self.prestacion != ''):            
+            self.selector_prestacion.set(self.prestacion)
         self.selector_prestacion.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
         odontologos = ["Militello", "Macua", "Ramirez"]
         self.selector_odontologo= ttk.Combobox(self.ventana_secundaria, state="readonly", values=odontologos, width=25, justify=CENTER, background="white")
         self.selector_odontologo.pack(pady=8)
         self.selector_odontologo.set("Odontólogo")
+        if (self.odontologo != ''):            
+            self.selector_odontologo.set(self.odontologo)
         self.selector_odontologo.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+      
+             
         
-        tiempo_frame = Frame(self.ventana_secundaria)
-        tiempo_frame.pack(pady=8)
+        self.button_frame = Frame(self.ventana_secundaria, bg="gray")
+        self.button_frame.pack(pady=10)
+        
+        Button(self.button_frame, text='confirm', command=self.guardar_turno, bg="#BDC1BE").grid(row=0, column=0, padx=10)
+        Button(self.button_frame, text='cancelar',  command=self.cancelar_turno, bg="#BDC1BE").grid(row=0, column=1, padx = 10)
+        
+    def guardar_turno(self):
+        #print(self.valores)
+        self.valores = {
+            "paciente": self.nombre_entry.get(),
+            "horario": self.horario,
+            "prestacion": self.selector_prestacion.get(),
+            "odontologo": self.selector_odontologo.get()
+        }   
+        style = ttk.Style()
+        if self.valores["paciente"] == "Paciente" or self.valores["prestacion"] == "Prestación" or self.valores["odontologo"] == "Odontologo":
+            style.configure("TCombobox", fieldbackground="red", background="white")
+            self.nombre_entry.configure(bg="red")
+            self.aviso = Label(self.button_frame, text="Complete la información", bg="#BDC1BE", fg="red", font="Helvetica 10")
+            self.aviso.grid(pady=10,row=1, column=0, columnspan=2, padx = 10)
+        else:
+            print('datos completos')    
 
-        horas = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-        self.selector_hora = ttk.Combobox(tiempo_frame, values=horas, state="readonly", justify=CENTER, background="white", width=10)
-        self.selector_hora.set("Hora")
-        self.selector_hora.grid(row=0, column=0)
+        # """ Reconfigure red zones if triggered """
+        # self.nombre_entry.configure(bg="white")
+        # style.configure("TCombobox", fieldbackground="white", background="white")    
+        # tiempo_frame = Frame(self.ventana_secundaria)
+        # tiempo_frame.pack(pady=8)
 
-        minutos = ["00","30"]
-        #minutos.extend([str(num * 10) for num in range(1, 6)])
-        self.selector_minuto = ttk.Combobox(tiempo_frame, state="readonly", values=minutos, justify=CENTER, background="white", width=10)
-        self.selector_minuto.set("00")
-        self.selector_minuto.grid(row=0, column=1, sticky=E)
-        self.selector_hora.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
-        self.selector_minuto.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        # horas = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        # self.selector_hora = ttk.Combobox(tiempo_frame, values=horas, state="readonly", justify=CENTER, background="white", width=10)
+        # self.selector_hora.set("Hora")
+        # self.selector_hora.grid(row=0, column=0)
+
+        # minutos = ["00","30"]
+        # #minutos.extend([str(num * 10) for num in range(1, 6)])
+        # self.selector_minuto = ttk.Combobox(tiempo_frame, state="readonly", values=minutos, justify=CENTER, background="white", width=10)
+        # self.selector_minuto.set("00")
+        # self.selector_minuto.grid(row=0, column=1, sticky=E)
+        # self.selector_hora.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
+        # self.selector_minuto.bind("<<ComboboxSelected>>", lambda e: self.ventana_secundaria.focus())
         
-        button_frame = Frame(self.ventana_secundaria, bg="gray")
-        button_frame.pack(pady=10)
         
-        Button(button_frame, text='confirm', command=self.agregar_turno, bg="#BDC1BE").grid(row=0, column=0, padx=10)
-        Button(button_frame, text='cancelar',  command=self.cancelar_turno, bg="#BDC1BE").grid(row=0, column=1, padx = 10)
         
         #valores = self.data['values'] #VALORES DE LA TABLA
         #
@@ -111,12 +148,23 @@ class Turno:
         self.frame_tabla = ttk.Frame(self.ventana_turno)
         #self.frame_tabla.grid(column=0,row=1)
         self.frame_tabla.grid(columnspan= 4, row= 0, sticky= 'nsew')
-        Label(self.frame_tabla, text="TURNOS").grid(columnspan= 4, column= 0, row= 0)
-        self.style = ttk.Style()
-# Configure a new Treeview Heading style
-        self.style.configure("Treeview.Heading", font=('Helvetica', 10, 'bold'), background="#D3D3D3")
-        self.tabla_turnos = ttk.Treeview(self.frame_tabla, columns= ("Horario", "Paciente", "Prestacion", "Odontologo"), show= 'headings', height=25, selectmode ='browse')
+        Label(self.frame_tabla, text="TURNOS", font=('Helvetica', 10, 'bold')).grid(columnspan= 4, column= 0, row= 0, pady=5)
+        
+        self.tabla_turnos = ttk.Treeview(self.frame_tabla, columns= ("Horario", "Paciente", "Prestacion", "Odontologo"), show= 'headings', height=27, selectmode ='browse')
         self.tabla_turnos.grid(column= 0, row= 2, columnspan= 4, sticky= 'nsew')
+        self.style = ttk.Style()
+        self.style.theme_use('clam')
+        
+        # Configure a new Treeview Heading style
+        self.style.configure("Treeview.Heading", font=('Helvetica', 10, 'bold'), background="gray")
+        self.style.configure("Treeview",            
+            foreground="black",
+            rowheight=20
+            )
+        
+        # Change selected color
+        self.style.map('Treeview', background=[('selected', 'green')])
+
         # Definir los encabezados de las columnas
         self.tabla_turnos.heading("Horario", text= "Horario")
         self.tabla_turnos.heading("Paciente", text= "Paciente")
@@ -128,11 +176,9 @@ class Turno:
         self.tabla_turnos.column("Paciente", width= 100)
         self.tabla_turnos.column("Prestacion", width= 100)
         self.tabla_turnos.column("Odontologo", width= 100)
-        #self.tabla_turnos.tag_configure('anotado', foreground='red')
-        self.cargar_turnos()        
+        
         # cargar filas al Treeview
-        #self.tabla_turnos.tag_configure('anotado', foreground='red')
-#        self.tabla_turnos.tag_configure('gray',background='gray')
+        self.cargar_turnos()        
         self.tabla_turnos.bind("<Double-1>", self.editar_turno)
        #self.frame_tabla.grid_columnconfigure(0, weight=1)
         
